205452639c79807a28a6c924d4c8fa7670630cb2
avanishsrivastava2907 <avanish.ranjan@quadbtech.com>
2024-10-07 17:56:14 +0530

    changes

--- src/claimlink_frontend/src/common/NftPayment.jsx
+++ src/claimlink_frontend/src/common/NftPayment.jsx
@@ -4,7 +4,7 @@ import MainButton from "./Buttons";
 import { toast } from "react-hot-toast";
 import { Button } from "@headlessui/react";
 import logo from "../assets/img/logiicpp.png";
-const coffeeAmount = 100_000_000; // 0.04 ICP in e8s
+const coffeeAmount = 100_000; // 0.04 ICP in e8s
 // ICP=
 // 1,000,000,000,000 cycles per ICP
 // 100,000,000 cycles
