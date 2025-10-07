5aa91777b575aa919ff93b10df485a7bb90ca8ab
Artur Shirokov <artur@origyn.ch>
2025-10-07 11:58:14 +0100

    Forgot to remove the old image-upload section references.

--- frontend/claimlink_dashboard/src/features/mint-certificate/index.ts
+++ frontend/claimlink_dashboard/src/features/mint-certificate/index.ts
@@ -3,7 +3,6 @@ export { CreateCertificatePage } from './components/create-certificate-page';
 export { CertificatesActions } from './components/certificates-actions';
 export { CertificatesGrid } from './components/certificates-grid';
 export { CollectionSection } from './components/collection-section';
-export { ImageUploadSection } from './components/image-upload-section';
 export { CompanyForm } from './components/company-form';
 export { PricingSidebar } from './components/pricing-sidebar';
 export { FormField } from './components/form-field';
