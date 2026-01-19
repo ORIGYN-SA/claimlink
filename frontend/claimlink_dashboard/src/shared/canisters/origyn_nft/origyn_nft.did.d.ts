b9c8218850872acafea207f0435853d34b1a5a4e
Artur Shirokov <artur@origyn.ch>
2026-01-19 15:29:41 +0000

    Updated TS interfaces to match the recent batch minting update.

--- frontend/claimlink_dashboard/src/shared/canisters/origyn_nft/origyn_nft.did.d.ts
+++ frontend/claimlink_dashboard/src/shared/canisters/origyn_nft/origyn_nft.did.d.ts
@@ -49,10 +49,13 @@ export interface Args_1 {
   'file_size' : bigint,
   'chunk_size' : [] | [bigint],
 }
-export interface Args_2 {
+export interface MintRequest {
   'metadata' : Array<[string, ICRC3Value]>,
   'memo' : [] | [Uint8Array | number[]],
   'token_owner' : Account,
+}
+export interface Args_2 {
+  'mint_requests' : Array<MintRequest>,
 }
 export interface Args_3 { 'authorized_principals' : Array<Principal> }
 export interface Args_4 { 'minting_authorities' : Array<Principal> }
