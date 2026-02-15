da0025c7d2fe3c5b4ff08fd1b2d19c435a44d3f9
Jeff Jafari <jeff@origyn.ch>
2026-02-15 19:22:41 +0400

    add get_collections_for_users query

--- backend/canisters/claimlink/impl/src/queries/get_collection_nfts.rs
+++ backend/canisters/claimlink/impl/src/queries/get_collection_nfts.rs
@@ -5,7 +5,8 @@ pub use claimlink_api::queries::get_collection_nfts::{
 #[ic_cdk::query(composite = true)]
 #[bity_ic_canister_tracing_macros::trace]
 pub async fn get_collection_nfts(args: GetCollectionNftsArgs) -> GetCollectionNftsResponse {
-    origyn_nft_canister_c2c_client::icrc7_tokens(args.canister_id, &(args.prev, args.take))
+    origyn_nft_canister_c2c_client::icrc7_tokens(args.canister_id, (args.prev, args.take))
         .await
-        .unwrap_or_default()
+        .unwrap_or((vec![],))
+        .0
 }
--- backend/canisters/claimlink/impl/src/queries/mod.rs
+++ backend/canisters/claimlink/impl/src/queries/mod.rs
@@ -2,10 +2,11 @@ mod get_collection_count;
 mod get_collection_info;
 mod get_collection_nfts;
 mod get_collections_by_owner;
-mod get_nft_details;
-// mod get_user_nfts; // TODO: Temporarily commented out - see get_user_nfts.rs for details
+mod get_collections_for_user;
 mod get_metrics;
+mod get_nft_details;
 mod get_templates_by_owner;
+mod get_user_nfts;
 mod http_request;
 mod list_all_collections;
 
@@ -13,8 +14,9 @@ pub use get_collection_count::*;
 pub use get_collection_info::*;
 pub use get_collection_nfts::*;
 pub use get_collections_by_owner::*;
+pub use get_collections_for_user::*;
 pub use get_nft_details::*;
-// pub use get_user_nfts::*; // TODO: Temporarily commented out - see get_user_nfts.rs for details
+//pub use get_user_nfts::*;
 pub use get_metrics::*;
 pub use get_templates_by_owner::*;
 pub use http_request::*;
